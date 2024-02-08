b9eb543f2aaf688ff84af96dd807900e74fd56ef
Shubham-Ftechiz <shubham.pal@ftechiz.com>
2024-02-08 17:12:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test Akshay
+          Learn React test Shubham
         </a>
       </header>
     </div>
